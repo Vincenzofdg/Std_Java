7e408a3bf72b3815fa2a0fda7957a3de8206c306
Vincenzofdg <vincenzofdg.dev@gmail.com>
2024-07-01 17:24:30 -0300

    metodo privete e public

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Film movie = new Film();
+
+        movie.name = "The Lord of the Rings: The Fellowship of the Ring";
+        movie.releaseYear = 2002;
+        movie.description = "Is renowned for its rich storytelling, complex characters, and detailed world-building. It has significantly influenced the fantasy genre and was adapted into a critically acclaimed film in 2001.";
+        movie.timeMinute = 178;
+        movie.plan = true;
+//        movie.rate = 30;
+//        movie.totalRate = 2;
+
+        movie.showFilmFile();
+
+        movie.rateMovie(70);
+        movie.rateMovie(40);
+        movie.rateMovie(67);
+        movie.rateMovie(59);
+
+
+        System.out.println(movie.getTotalRate());
+        System.out.println(movie.rateAvg());
+    }
+}
